4b4a0e17b1cd9950c892166465dc54172d8fcc28
Yerkebay Meirzhan <erkebaevmejrzan6@gmail.com>
2026-04-11 15:48:39 +0500

    feat: implement Payment gRPC server with logging interceptor

---
+++ payment-service/internal/transport/grpc/interceptor.go
@@ -0,0 +1,24 @@
+package grpc
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func LoggingUnaryInterceptor() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		start := time.Now()
+		resp, err := handler(ctx, req)
+		duration := time.Since(start)
+		log.Printf("[gRPC] method=%s duration=%s err=%v", info.FullMethod, duration, err)
+		return resp, err
+	}
+}
